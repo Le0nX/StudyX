33843219e8f0ff8af96e8af47aed0e22b6a131a2
zorro <zorro@localhost.localdomain>
2016-11-30 17:01:48 +0300

    playing with gtk

---
+++ GTK/hello_gtk.c
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]){
+
+	GtkWidget *window;
+
+	gtk_init(&argc, &argv); /*initilize Gtk and all of its libs*/
+	
+	
+	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title (GTK_WINDOW (window), "Test frame");
+	gtk_widget_show (window);
+
+	/*control over the main loop*/	
+	gtk_main ();
+
+	return 0;
+}
