d4322d76bdc7aa1490da7574723467824031d537
Le0nX <cloudmindone@gmail.com>
2017-06-06 16:23:00 +0300

    realization of structure

--- Data_Struct/list.c
+++ Data_Struct/list.c
@@ -1,1 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
 
+#include "list.h"
+
+
+/*********************
+* initalize the list *
+*********************/
+
+void list_init(List *list, void (*destroy)(void *data))
+{
+	list->size = 0;
+	list->destroy = destroy;
+	list->head = NULL;
+	list->tail = NULL;
+	
+	return;
+}
+
+
+void list_destroy(List *list)
+{
+	void *data;
+	
+	while (list_size(list) > 0) {
+		if (list_rem_next(list, NULL, (void **)&data) == 0 && 
+												list->destroy != NULL) 
+			list->destroy(data);		
+	}
+	
+	memset(list, 0, sizeof(list));
+}
+
+int list_ins_next(List *list, ListElmt *element, const void *data) 
+{
+	ListElmt *new_element;
+	
+	/* allocate storage  for the element */
+	
+	if ((new_element = (ListElmt *)malloc(sizeof(ListElmt))) == NULL)
+		return -1;
+	
+	
+	/* insert the elemeint into the list. */
+	
+	
+}
