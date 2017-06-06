6af08f6be6fdbac41778b1ffe73dada6b03737e2
Le0nX <cloudmindone@gmail.com>
2017-06-06 13:05:31 +0300

    linked list interface

---
+++ Data_Struct/list.h
@@ -0,0 +1,54 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+/************************************
+* define a struct for list elements *
+************************************/
+
+
+typedef struct ListElmt_ {
+	
+	void 		  *data;
+	void ListElmt *next;
+	
+} ListElmt;
+
+
+/***********************************
+* define a struct for linked lists *
+***********************************/
+
+typedef struct List_ {
+	
+	int 		size;
+	int 		(*match)(const void *key1, const void *key2);
+	void 		(*destroy)(void *data);
+	ListElmt	*head;
+	ListElmt	*tail;
+	
+} List;
+
+
+/***********************************
+* ------- Public Interface ------- *
+***********************************/
+
+
+void list_init(List *list, void (*destroy)(void *data));
+
+void list_destroy(List *list);
+
+int list_ins_next(List *list, ListElmt *element, const void *data);
+
+int list_rem_next(List *list, ListElmt *element, void **data);
+
+#define list_size(list) ((list)->size)
+
+#define list_head(list) ((list)->head)
+
+#define list_tail(list) ((list)->tail)
+
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+
