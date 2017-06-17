da8c59fabf911f402d0260520b03a3ba989785dd
Le0nX <cloudmindone@gmail.com>
2017-06-17 14:13:17 +0300

    [CORE] started implementation of DList

--- Data_Struct/dlist.c
+++ Data_Struct/dlist.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "dlist.h"
+
+
+void dlist_init(DList *list, void (*destroy)(void *data))
+{
+	list->size = 0;
+	list->head = NULL;
+	list->tail = NULL;
+	
+	list->destroy = destroy;
+	
+	return;
+}
+
 
